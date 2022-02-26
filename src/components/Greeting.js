6ec446d27d535979642904ef159f974cc3ac27a1
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-02-26 22:04:59 +0200

    Implement greetings component

---
+++ src/components/Greeting.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchmessage } from '../redux/message/api';
+
+const Greeting = () => {
+  const message = useSelector((state) => state.messageReducer.message);
+  // console.log('message => ', message);
+
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(fetchmessage());
+    console.log('hi there');
+  };
+
+  return (
+    <div className="wrapper">
+      <h1>Greeting</h1>
+      <div className="message">
+        Message:
+        {' '}
+        {message}
+      </div>
+      <button type="button" onClick={handleClick}>GREETING</button>
+    </div>
+  );
+};
+
+export default Greeting;
